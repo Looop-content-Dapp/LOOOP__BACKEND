dca6676c2e2db5fbd27733471246250f7ac3cbb6
TB <test@tests-MacBook-Pro.local>
2024-10-01 08:48:54 +0100

    ft: delete user endpoint

--- routes/genres.route.js
+++ routes/genres.route.js
@@ -3,11 +3,14 @@ const {
   deleteGenre,
   createAGenre,
   getGenres,
+  getUserGenres,
 } = require("../controller/genre.controller");
 
 const genreRoute = express.Router();
 
+genreRoute.get("/usergenre/:userId", getUserGenres);
 genreRoute.get("/getgenres", getGenres);
+
 genreRoute.post("/creategenre", createAGenre);
 genreRoute.delete("/:genreId", deleteGenre);
 
