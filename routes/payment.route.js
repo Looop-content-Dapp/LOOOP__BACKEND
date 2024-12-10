5618bd55a9e0b141cdb347005de04ad41973ee5b
TEMHITHORPHE <tehmhithorphe@gmail.com>
2024-12-10 11:41:12 +0100

    update: verify endpoint

--- routes/payment.route.js
+++ routes/payment.route.js
@@ -1,5 +1,6 @@
 
 import { Router } from 'express';
+import { paystackWebhookHandler, verifyPaysatckTransaction } from '../controller/paystack.controller';
 const paymentRouter = Router();
 
 
@@ -9,5 +10,8 @@ paymentRouter.post('/init', paymentController);
 // handles webhook transaction events sent from paystack.
 paymentRouter.post('/paystack/webhook/', paystackWebhookHandler);
 
+// verify paystack transaction status
+// TODO: Endpoint currently susceptible to a form of IDOR attack, fix!!!.
+paymentRouter.post('/paystack/transaction/verify', verifyPaysatckTransaction);
 
 export default paymentRouter;
