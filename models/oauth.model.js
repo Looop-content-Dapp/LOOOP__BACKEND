f7e66d2f8529358c068614632af1825f64986297
TEMHITHORPHE <tehmhithorphe@gmail.com>
2024-12-15 16:05:12 +0100

    add: oauth providers model

---
+++ models/oauth.model.js
@@ -0,0 +1,34 @@
+import { Schema, SchemaTypes, model } from "mongoose";
+
+// https://datatracker.ietf.org/doc/html/rfc6749#section-5.1
+// "access_token": "2YotnFZFEjr1zCsicMWpAA",
+// "token_type": "example",
+// "expires_in": 3600,
+// "refresh_token": "tGzv3JOkF0XG5Qx2TlKWIA",
+// "example_parameter": "example_value"
+const OAuthTokenObjectSchema = new Schema({
+    access_token: {
+        type: SchemaTypes.String,
+        required: true
+    },
+    token_type: {
+        type: String,
+        enum: ["bearer", "mac"],
+        required: true,
+        lowercase: true
+    },
+    scope: {    //  https://datatracker.ietf.org/doc/html/rfc6749#section-3.3
+        type: SchemaTypes.String,
+        required: true
+    },
+    expires_in: {
+        type: SchemaTypes.String,
+        required: false
+    },
+    refresh_token: {
+        type: SchemaTypes.String,
+        required: false
+    },
+},
+    { strict: false }   // Just in case we have providers that send custom fields in token response.
+)
