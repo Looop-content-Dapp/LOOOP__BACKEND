9c7752a15a5779994f263bb0b47411107dacc0b4
Senninseyi <seyi.oyebamiji@gmail.com>
2025-02-20 17:31:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -52,9 +52,9 @@ app.use("/api/admin", adminRouter);
 
 const PORT = process.env.NODE_ENV === "production" ? process.env.PORT : 8000;
 const mongoURI =
-  // process.env.NODE_ENV !== "production"
-  //   ? "mongodb://localhost:27017/"
-  //   : process.env.MONGODB_URI ||
+  process.env.NODE_ENV !== "production"
+    ? "mongodb://localhost:27017/"
+    : process.env.MONGODB_URI ||
       "mongodb+srv://looopMusic:Dailyblessing@looopmusic.a5lp1.mongodb.net/?retryWrites=true&w=majority&appName=LooopMusic";
 
 (async () => {
