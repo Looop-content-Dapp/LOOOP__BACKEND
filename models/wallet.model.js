5b2be8b18f7e76cf20fb6d8bdf2f80f59df5f6ac
Senninseyi <seyi.oyebamiji@gmail.com>
2025-03-06 18:12:53 +0100

    CHORE: update app

---
+++ models/wallet.model.js
@@ -0,0 +1,22 @@
+import { Schema, model } from "mongoose";
+
+const walletSchema = new Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    userId: { type: String, required: false, unique: true },
+    xion: {
+      type: {
+        address: { type: String, required: true },
+        encryptedMnemonic: { type: String, required: true },
+        iv: { type: String, required: true },
+        salt: { type: String, required: true },
+      },
+      required: true,
+    },
+    recoveryToken: { type: String, default: null },
+    recoveryTokenExpiry: { type: Date, default: null },
+  },
+  { timestamps: true }
+);
+
+export const Wallet = model("wallets", walletSchema);
